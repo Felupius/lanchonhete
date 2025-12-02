077655445bef254331844c5141d7228fa2dad51b
themax6600 <max.2935@aluno.pr.senac.br>
2025-12-02 10:48:40 -0300

    fhj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-
+import sesc from "../assets/sesc.png";
 export default function Header({ usuario, handleSair, quantidadeCarrinho }) {
 
   const [menuAberto, setMenuAberto] = useState(false);
@@ -12,13 +12,13 @@ export default function Header({ usuario, handleSair, quantidadeCarrinho }) {
       <div className="flex items-center gap-4">
         <Link to="/" className="group">
           <img
-            src="../assets/sesc.png"
+            src={sesc}
             className="h-10 transition-transform duration-300 group-hover:scale-110 drop-shadow-lg"
           />
         </Link>
         <Link to="/" className="group">
           <img
-            src="../assets/senac.png"
+            src="./src/assets/senac.png"
             className="h-10 transition-transform duration-300 group-hover:scale-110 drop-shadow-lg"
           />
         </Link>
